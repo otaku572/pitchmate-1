56d0e5002a9fd473076fa082c50caf1a091e858a
otaku572 <otakudeals4688@gmail.com>
2025-09-17 20:36:14 +0530

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../api/axios";
 
 interface UserData {
   email: string;
